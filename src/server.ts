9c78d28e01b9123c2f29a3f99cc1af4894661cde
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2025-12-07 21:02:17 +0600

    Booking cancelled successfully for Customer side.

--- src/server.ts
+++ src/server.ts
@@ -78,6 +78,7 @@ app.get('/', (req: Request, res: Response) => {
 
 
 // Users CRUD
+
 // create user
 app.post("/api/v1/auth/signup", async (req: Request, res: Response) => {
 
@@ -207,6 +208,7 @@ app.delete("/api/v1/users/:userId", async (req: Request, res: Response) => {
 
 
 // vehicles CRUD
+
 // add vehicles
 app.post("/api/v1/vehicles", async (req: Request, res: Response) => {
     const { vehicle_name, type, registration_number, daily_rent_price, availability_status } = req.body;
@@ -365,8 +367,7 @@ app.delete("/api/v1/vehicles/:vehicleId", async (req: Request, res: Response) =>
 
 // Bookings Crud
 
-
-
+// create booking
 app.post("/api/v1/bookings", async (req: Request, res: Response) => {
     try {
         const { customer_id, vehicle_id, rent_start_date, rent_end_date } = req.body;
@@ -417,13 +418,109 @@ app.post("/api/v1/bookings", async (req: Request, res: Response) => {
     }
 });
 
+// get all bookings
 
+app.get("/api/v1/bookings", async (req: Request, res: Response) => {
+    const getBookingsQuery = `
+        SELECT 
+            b.id AS booking_id, 
+            b.rent_start_date, 
+            b.rent_end_date, 
+            b.total_price, 
+            b.status,
+            u.name AS customer_name, 
+            v.vehicle_name, 
+            v.daily_rent_price
+        FROM Bookings b
+        JOIN Users u ON b.customer_id = u.id
+        JOIN Vehicles v ON b.vehicle_id = v.id
+        ORDER BY b.create_at DESC
+    `;
 
+    try {
+        const result = await pool.query(getBookingsQuery);
 
+        if (result.rows.length === 0) {
+            return res.status(200).json({
+                success: true,
+                message: "No bookings found",
+                data: []
+            });
+        }
 
+        res.status(200).json({
+            success: true,
+            message: "Bookings retrieved successfully",
+            data: result.rows
+        });
 
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to retrieve bookings"
+        });
+    }
+});
 
+// update bookings status
+app.put("/api/v1/bookings/:bookingId", async (req: Request, res: Response) => {
+    const { bookingId } = req.params;
+    const { status } = req.body || {};
 
+    if (!status) {
+        return res.status(400).json({ success: false, message: "Status is required" });
+    }
+
+    try {
+        const bookingCheck = await pool.query(`SELECT * FROM Bookings WHERE id = $1`, [bookingId]);
+        if (bookingCheck.rowCount === 0) {
+            return res.status(404).json({ success: false, message: "Booking not found" });
+        }
+
+        const currentBooking = bookingCheck.rows[0];
+
+        if (status === 'cancelled') {
+            const today = new Date();
+            today.setHours(0, 0, 0, 0);
+
+            const startDate = new Date(currentBooking.rent_start_date);
+            startDate.setHours(0, 0, 0, 0);
+
+            if (today > startDate) {
+                return res.status(400).json({
+                    success: false,
+                    message: "Cannot cancel. Rent period has already started."
+                });
+            }
+        }
+
+        await pool.query('BEGIN');
+
+        const updatedBookingResult = await pool.query(
+            `UPDATE Bookings SET status = $1, updated_at = NOW() WHERE id = $2 RETURNING *`,
+            [status, bookingId]
+        );
+
+        if (status === 'returned' || status === 'cancelled') {
+            await pool.query(
+                `UPDATE Vehicles SET availability_status = 'available' WHERE id = $1`,
+                [currentBooking.vehicle_id]
+            );
+        }
+
+        await pool.query('COMMIT');
+
+        res.status(200).json({
+            success: true,
+            message: status === 'cancelled' ? "Booking cancelled successfully" : "Booking marked as returned. Vehicle is now available",
+            data: updatedBookingResult.rows[0]
+        });
+
+    } catch (err: any) {
+        if (pool) await pool.query('ROLLBACK');
+        res.status(500).json({ success: false, message: err.message });
+    }
+});
 
 
 
